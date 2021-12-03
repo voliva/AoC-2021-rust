7a2350651cb8fec8a3d206fdcd5908dfbbc9b83b
Víctor Oliva <olivarra1@gmail.com>
2021-12-03 22:05:34 +0100

    cleanup day 3

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -1,5 +1,6 @@
 use super::Solver;
 use itertools::partition;
+use std::convert::TryInto;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::vec::Vec;
@@ -7,26 +8,30 @@ use std::vec::Vec;
 pub struct Problem;
 
 impl Solver for Problem {
-    type Input = Vec<usize>;
+    type Input = (usize, Vec<usize>);
     type Output1 = usize;
     type Output2 = usize;
 
     fn read_input(&self, file_reader: BufReader<&File>) -> Self::Input {
-        file_reader
-            .lines()
-            .filter_map(|x| x.ok())
-            .map(|line| usize::from_str_radix(line.as_str(), 2))
-            .filter_map(|x| x.ok())
-            .collect()
+        let mut lines = file_reader.lines().peekable();
+
+        let length = lines.peek().unwrap().as_ref().unwrap().len();
+
+        (
+            length,
+            lines
+                .filter_map(|x| x.ok())
+                .map(|line| usize::from_str_radix(line.as_str(), 2))
+                .filter_map(|x| x.ok())
+                .collect(),
+        )
     }
 
-    fn solve_first(&self, input: &Self::Input) -> Result<Self::Output1, String> {
-        let mut vec = Vec::from([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-
-        let bits = vec.len();
+    fn solve_first(&self, (bits, input): &Self::Input) -> Result<Self::Output1, String> {
+        let mut vec = vec![0; *bits];
 
         for n in input {
-            for i in 0..bits {
+            for i in 0..*bits {
                 if n & (0x01 << i) > 0 {
                     vec[i] = vec[i] + 1
                 }
@@ -36,7 +41,7 @@ impl Solver for Problem {
         let n = input.len();
         let mut epsilon = 0;
         let mut gamma = 0;
-        for i in 0..bits {
+        for i in 0..*bits {
             let mask = 0x01 << i;
             if vec[i] > n / 2 {
                 gamma = gamma | mask
@@ -45,29 +50,27 @@ impl Solver for Problem {
             }
         }
 
-        println!("{} {}", epsilon, gamma);
         Ok(epsilon * gamma)
     }
 
-    fn solve_second(&self, input: &Self::Input) -> Result<Self::Output2, String> {
-        let bits: isize = 12;
+    fn solve_second(&self, (bits, input): &Self::Input) -> Result<Self::Output2, String> {
         let mut clone = input.clone();
 
         let main_split = partition(&mut clone.iter_mut(), |x| x & (0x01 << (bits - 1)) >= 1);
+        let next_bits = (bits - 2).try_into().unwrap();
 
         let (oxygen, co2) = if main_split > input.len() / 2 {
             (
-                calculate_oxygen(&mut clone[..main_split], bits - 2),
-                calculate_co2(&mut clone[main_split..], bits - 2),
+                calculate_oxygen(&mut clone[..main_split], next_bits),
+                calculate_co2(&mut clone[main_split..], next_bits),
             )
         } else {
             (
-                calculate_oxygen(&mut clone[main_split..], bits - 2),
-                calculate_co2(&mut clone[..main_split], bits - 2),
+                calculate_oxygen(&mut clone[main_split..], next_bits),
+                calculate_co2(&mut clone[..main_split], next_bits),
             )
         };
 
-        println!("{} {}", oxygen, co2);
         Ok(oxygen * co2)
     }
 }
