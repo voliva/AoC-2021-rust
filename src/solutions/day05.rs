425537a25e708669ae92d34f3c97a70448b51349
Víctor Oliva <olivarra1@gmail.com>
2021-12-05 10:35:32 +0100

    cleanup day 5

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -5,90 +5,6 @@ use std::str::FromStr;
 
 pub struct Problem;
 
-#[derive(Debug, Clone)]
-struct Point {
-    x: usize,
-    y: usize,
-}
-
-impl FromStr for Point {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut split = s.split(",");
-        Ok(Point {
-            x: split.next().unwrap().parse().unwrap(),
-            y: split.next().unwrap().parse().unwrap(),
-        })
-    }
-}
-
-#[derive(Debug)]
-pub struct Line {
-    start: Point,
-    end: Point,
-}
-
-impl FromStr for Line {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut split = s.split(" -> ");
-        Ok(Line {
-            start: split.next().unwrap().parse().unwrap(),
-            end: split.next().unwrap().parse().unwrap(),
-        })
-    }
-}
-
-impl Line {
-    fn is_straight(&self) -> bool {
-        (self.start.x == self.end.x) || (self.start.y == self.end.y)
-    }
-    fn run_through<F>(&self, mut f: F)
-    where
-        F: FnMut(&Point) -> (),
-    {
-        let x_grad = (self.end.x as isize) - (self.start.x as isize);
-        let x_grad_u = if x_grad == 0 {
-            0
-        } else {
-            x_grad / x_grad.abs()
-        };
-        let y_grad = (self.end.y as isize) - (self.start.y as isize);
-        let y_grad_u = if y_grad == 0 {
-            0
-        } else {
-            y_grad / y_grad.abs()
-        };
-
-        let mut p = Point {
-            x: self.start.x,
-            y: self.start.y,
-        };
-
-        if self.start.x == self.end.x {
-            while p.y != ((self.end.y as isize) + y_grad_u) as usize {
-                f(&p);
-                p.y = ((p.y as isize) + y_grad_u) as usize;
-            }
-        } else if self.start.y == self.end.y {
-            while p.x != ((self.end.x as isize) + x_grad_u) as usize {
-                f(&p);
-                p.x = ((p.x as isize) + x_grad_u) as usize;
-            }
-        } else {
-            while (p.x != ((self.end.x as isize) + x_grad_u) as usize)
-                && (p.y != ((self.end.y as isize) + y_grad_u) as usize)
-            {
-                f(&p);
-                p.x = ((p.x as isize) + x_grad_u) as usize;
-                p.y = ((p.y as isize) + y_grad_u) as usize;
-            }
-        }
-    }
-}
-
 impl Solver for Problem {
     type Input = Vec<Line>;
     type Output1 = isize;
@@ -144,4 +60,89 @@ fn get_max(vec: &Vec<Line>) -> Point {
         acc.y = std::cmp::max(acc.y, std::cmp::max(line.start.y, line.end.y));
         return acc;
     })
+}
+
+#[derive(Debug, Clone)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+impl FromStr for Point {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split(",");
+        Ok(Point {
+            x: split.next().unwrap().parse().unwrap(),
+            y: split.next().unwrap().parse().unwrap(),
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Line {
+    start: Point,
+    end: Point,
+}
+
+impl FromStr for Line {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split(" -> ");
+        Ok(Line {
+            start: split.next().unwrap().parse().unwrap(),
+            end: split.next().unwrap().parse().unwrap(),
+        })
+    }
+}
+
+impl Line {
+    fn is_straight(&self) -> bool {
+        (self.start.x == self.end.x) || (self.start.y == self.end.y)
+    }
+    fn run_through<F>(&self, mut f: F)
+    where
+        F: FnMut(&Point) -> (),
+    {
+        let x_grad = Gradient::new(self.start.x, self.end.x);
+        let y_grad = Gradient::new(self.start.y, self.end.y);
+
+        let mut p = Point {
+            x: self.start.x,
+            y: self.start.y,
+        };
+
+        if self.start.x == self.end.x {
+            while p.y != y_grad.apply(self.end.y) {
+                f(&p);
+                p.y = y_grad.apply(p.y);
+            }
+        } else if self.start.y == self.end.y {
+            while p.x != x_grad.apply(self.end.x) {
+                f(&p);
+                p.x = x_grad.apply(p.x);
+            }
+        } else {
+            while (p.x != x_grad.apply(self.end.x)) && (p.y != y_grad.apply(self.end.y)) {
+                f(&p);
+                p.x = x_grad.apply(p.x);
+                p.y = y_grad.apply(p.y);
+            }
+        }
+    }
+}
+
+struct Gradient(isize);
+
+impl Gradient {
+    pub fn new(start: usize, end: usize) -> Self {
+        let diff = (end as isize) - (start as isize);
+        Gradient(if diff == 0 { 0 } else { diff / diff.abs() })
+    }
+
+    pub fn apply(&self, value: usize) -> usize {
+        ((value as isize) + self.0) as usize
+    }
 }
